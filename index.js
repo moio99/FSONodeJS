2227ad75c001cc6c10ec5b0c973ec908c05b0504
Frederico <fozg@gft.com>
2025-02-18 14:59:01 +0100

    3.3 step 3

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ let persons = [
     number: "39-23-6423122",
     id: "3"
   },
+  {
+    name: "Claude Monet",
+    number: "40-23-6423122",
+    id: "4"
+  },
+  {
+    name: "Paul Cézanne",
+    number: "50-23-6423122",
+    id: "5"
+  },
 ]
 
 app.get('/info', (request, response) => {
@@ -33,6 +43,18 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const person = persons.find(person => person.id === request.params.id)
+  console.log(person)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.statusMessage = `No person has been found with id ${request.params.id}`
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
