5505084b977a4453a61a62f616cbc3ed4c411dac
Frederico <fozg@gft.com>
2025-02-20 13:10:46 +0100

    3.19 step 7

--- index.js
+++ index.js
@@ -18,28 +18,40 @@ app.use(express.static('dist'))
 app.use(cors())
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
 
   if (error.name === 'CastError') {
+    console.error('Error type: CastError')
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    console.error('Error type: ValidationError')
+    return response.status(400).json({ error: 'The name must be greater than 3 characters and the number greater than 5' })
+  } else {
+    console.error('Error type: other')
+    console.error(error.message)
+  }
 
   next(error)
 }
 
-// este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
-app.use(errorHandler)
 
 mongoose.set('strictQuery',false)
 mongoose.connect(process.env.MONGODB_URI)
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
 })
 const Person = mongoose.model('Person', personSchema)
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
   Person.find({})
     .then(people => {
       const date = new Date().toString()
@@ -48,7 +60,7 @@ app.get('/info', (request, response) => {
     .catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({})
     .then(people => {
       const peopleFront = people.map(p  => ({name: p.name, number: p.number, id: p._id,}))
@@ -75,7 +87,7 @@ app.get('/api/persons/:id', async (request, response, next) => {
   .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   Person.findByIdAndDelete(id)
     .then(person => {
@@ -94,7 +106,7 @@ app.delete('/api/persons/:id', (request, response) => {
     })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   if (!body) {
     return response.status(400).json({ 
@@ -121,10 +133,7 @@ app.post('/api/persons', (request, response) => {
       }
       response.json(personResponse)
     })
-    .catch(error => {
-      console.log(error)
-      next(error)
-    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -143,14 +152,17 @@ app.put('/api/persons/:id', (request, response, next) => {
     _id: id
   })
 
-  Person.findByIdAndUpdate(id, person, { new: true })
+  Person.findByIdAndUpdate(id, person, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
 
+// este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+app.use(errorHandler)
+
 const PORT = 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`O servidor está a escoitar no porto ${PORT}`);
-});
+})
