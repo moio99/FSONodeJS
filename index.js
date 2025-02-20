a136188b20188b46c40869bdb2b6dad9c9da8cc5
Frederico <fozg@gft.com>
2025-02-20 15:38:55 +0100

    added log

--- index.js
+++ index.js
@@ -177,5 +177,7 @@ app.use(errorHandler)
 
 const PORT = 3000;
 app.listen(PORT, '0.0.0.0', () => {
+  console.log(process.env.MONGODB_URI.toString().substring(0,7) );
+  console.log(process.env.MONGODB_URI.toString().substring(0,23) );
   console.log(`O servidor está a escoitar no porto ${PORT}`);
 })
