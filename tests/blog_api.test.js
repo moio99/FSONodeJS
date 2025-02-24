e0793f5da31df8c4cc6f08ddbf36e4a26100fae5
Frederico <fozg@gft.com>
2025-02-24 14:37:15 +0100

    4.4 step 2

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -8,3 +8,42 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+
+  const blogList = [
+    {
+      title: 'Título01',
+      author: 'Nome da pessoa para o 01',
+      url: 'umha direiçom 01',
+      likes: 2
+    },
+    {
+      title: 'Título02',
+      author: 'Nome da pessoa para o 02',
+      url: 'umha direiçom 02',
+      likes: 5
+    }
+  ]
+
+  test('when list is null, return 0', () => {
+    const result = listHelper.totalLikes(null)
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has not blogs, return 0', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const newList = [blogList[0]]
+    const result = listHelper.totalLikes(newList)
+    assert.strictEqual(result, newList[0].likes)
+  })
+
+  test('when list has several blogs, return de sum', () => {
+    const result = listHelper.totalLikes(blogList)
+    assert.strictEqual(result, 7)
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let result = 0
+  if (blogs) {
+    result = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  }
+  return result
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
