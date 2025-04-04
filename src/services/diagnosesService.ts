ce038b3c8f8a8e2a3322b24bb76277c3697e610b
Frederico <fozg@gft.com>
2025-04-04 14:52:23 +0200

    9.24 step 5

--- src/services/diagnosesService.ts
+++ src/services/diagnosesService.ts
@@ -6,11 +6,17 @@ const getEntries = () => {
   return diagnosis;
 };
 
+const getDiagnosis = (code: string): Diagnosis | undefined => {
+  const element = diagnosis.find(d => d.code === code);
+  return element;
+};
+
 const addEntry = () => {
   return null;
 };
 
 export default {
   getEntries,
+  getDiagnosis,
   addEntry
 };
